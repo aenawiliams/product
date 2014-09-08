03be51063e274e8baa4ad0b2c376147a1da692db
aqsa <aqsa.qt@gmail.com>
2014-09-08 12:06:55 +0500

    Initialize project

---
+++ json.php
@@ -0,0 +1,124 @@
+[ 
+{ 
+    "cName" : "Alfreds Futterkiste",
+    "City" : "Berlin",
+    "Country" : "Germany",
+    "carName" : "Ferrari",
+    "picture": "ferari.jpg"
+    
+      }, 
+      {
+    "cName" : "Berglunds snabbköp",
+    "City" : "Luleå",
+    "Country" : "Sweden",
+    "carName" : "Range Rover",
+    "picture": "rangerover.jpg"
+   } ,
+     {
+    "cName" : "Ayla Khan",
+    "City" : "Mexico",
+    "Country" : "Mexico",
+    "carName" : "Porsche",
+    "picture": "porsche.jpg"
+   } ,
+   {
+"cName" : "Ernst Handel",
+"City" : "Graz",
+"Country" : "Austria",
+"carName" : "Lamborghini",
+"picture": "lamborghini.jpg"
+},
+{
+"cName" : "FISSA Fabrica",
+"City" : "Madrid",
+"Country" : "Spain",
+ "carName" : "v8 Supercars",
+    "picture": "v8.jpg"
+
+},
+{
+"cName" : "John Smith",
+"City" : "Barcelona",
+"Country" : "Spain",
+ "carName" : "Glardo",
+    "picture": "glardo.jpg"
+
+},
+{
+"cName" : "Island Trading",
+"City" : "Cowes",
+"Country" : "UK",
+"carName" : "Mercedes",
+"picture": "mercedes.jpg"
+},
+{
+"cName" : "Koniglich Essen",
+"City" : "Brandenburg",
+"Country" : "Germany",
+"carName" : "Ford",
+"picture": "ford.jpg"
+},
+{
+"cName" : "Laughing Bacchus Wine Cellars",
+"City" : "Vancouver",
+"Country" : "Canada",
+"carName" : "Land Rover",
+"picture": "landrover.jpg"
+},
+{
+"cName" : "Magazzini Alimentari Riuniti",
+"City" : "Bergamo",
+"Country" : "Italy",
+"carName" : "Land Cruiser",
+"picture": "landcruiser.jpg"
+},
+{
+"cName" : "William",
+"City" : "London",
+"Country" : "UK",
+"carName" : "Nisan Juke",
+"picture": "nisanjuke.jpg"
+},
+{
+"cName" : "Paris specialites",
+"City" : "Paris",
+"Country" : "France",
+"carName" : "Mitsubishi Lancer",
+"picture": "mitsubishilancer.jpg"
+},
+{
+"cName" : "Rattlesnake Canyon Grocery",
+"City" : "Albuquerque",
+"Country" : "USA",
+"carName" : "Toyota Camry",
+"picture": "toyotacamry.jpg"
+},
+{
+"cName" : "Simons bistro",
+"City" : "København",
+"Country" : "Denmark",
+"carName" : "Toyota Supra",
+"picture": "toyotasupra.jpg"
+},
+{
+"cName" : "The Big Cheese",
+"City" : "Portland",
+"Country" : "USA",
+"carName" : "Chevrolet",
+"picture": "chevrolet.jpg"
+},
+{
+"cName" : "Vaffeljernet",
+"City" : "Arhus",
+"Country" : "Denmark",
+"carName" : "Chevrolet Corvette",
+"picture": "chevroletcorvette.jpg"
+},
+{
+"cName" : "Wolski Zajazd",
+"City" : "Warszawa",
+"Country" : "Poland",
+"carName" : "Jaguar",
+"picture": "jaguar.jpg"
+}
+   ]
